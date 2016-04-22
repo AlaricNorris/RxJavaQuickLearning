c481c7376d7b7163a2a0af729f00e8711cce3594
AlaricNorris <norris.sly@gmail.com>
2016-04-22 15:06:34 +0800

    啊啊

---
+++ RxjavaQuickStudy/RxJavaQuickLearning/app/src/main/java/com/alaric/norris/study/rxjava/LActivity.java
@@ -0,0 +1,26 @@
+/**
+ *  LActivity
+ *  com.alaric.norris.study.rxjava
+ *  Function:       ${TODO} 
+ *  date            author
+ *  *****************************************************
+ *  2016/4/22         AlaricNorris
+ *  Copyright (c) 2016, TNT All Rights Reserved.
+ */
+package com.alaric.norris.study.rxjava;
+
+import android.support.v7.app.AppCompatActivity;
+/**
+@formatter:off ClassName:      LActivity
+@formatter:off Function:       ${TODO}  ADD FUNCTION
+@formatter:off Contact:        Norris.sly@gmail.com
+@formatter:off @author         AlaricNorris 
+@formatter:off @version        Ver 1.0   
+@formatter:off @since          I used to be a programmer like you, then I took an arrow in the knee
+@formatter:off ***************************************************************************************************
+@formatter:off Modified By     AlaricNorris     2016/4/22    14:52
+@formatter:off Modifications:  ${TODO}
+@formatter:off ***************************************************************************************************
+ */
+public class LActivity extends AppCompatActivity{
+}
